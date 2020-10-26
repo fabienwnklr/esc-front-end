84edc83af723577fc6c879536986451034391dfd
fabien <fabien.winkler@outlook.fr>
2020-10-26 17:06:58 +0100

    Feat : début mise en place page admin

---
+++ src/views/admin/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span style="display:none;"></span>
+    <h1 class="text-center ma-10">Admin dashboad</h1>
+    <router-link to="/admin/add-game">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.$on('logged', () => console.log("logged"))
+  }
+}
+</script>
+
+<style>
+
+</style>
