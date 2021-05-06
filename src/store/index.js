a5487d851ebf0953e86a92a6fd3b8bcf6d4b3273
Fabien Winkler <fabien.winkler@outlook.fr>
2021-05-06 20:24:15 +0200

    [Fix] yarn.lock & centralisation store

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import app from "./modules/app";
-import menu from "./modules/menu";
-import user from "./modules/user";
+import modules from "./modules";
 
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
-  modules: {
-    app,
-    menu,
-    user
-  },
+  modules,
   strict: debug
 });
